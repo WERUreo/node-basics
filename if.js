a254328a578fcacaae2433d7c47fd4458833e4db
WERUreo <kelii.d.martin@gmail.com>
2016-01-09 13:26:38 -0500

    lecture 11

---
+++ if.js
@@ -0,0 +1,6 @@
+var averageGrade = 92;
+
+if (averageGrade > 70)
+{
+    console.log("You passed the class! Way to go!");
+}
---
+++ numbers.js
@@ -0,0 +1,5 @@
+var age = 40;
+var results;
+
+results = age + 6;
+console.log(results);
